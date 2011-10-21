431eda2ac22c25fbc1a6be63ae70a4b06e0fb30d
Ivan Slijepcevic <islijepcevic@gmail.com>
2011-10-21 18:09:15 +0200

    regdef removed for real

---
+++ generator/regularexpression.py
@@ -0,0 +1,20 @@
+'''This is the module that holds the implementation of regular expressions
+
+created: 11. 10. 2011
+Ivan Slijepcevic
+'''
+
+class RegularExpression():
+    '''class for handling regular expressions'''
+    
+    def __init__( self, expression ):
+        '''constructor
+        
+        arguments:
+        expression - the regular expression itself
+        '''
+        
+        pass
+    
+    def get_rid_of_regdef( self, regdef ):
+        pass
