da547a4eba1121a646364e0afa0cbaa6522987b7
makkrnic <makkrnic@gmail.com>
2011-10-29 18:25:49 +0200

    Implemented AnalyzerRule class

--- generator/analyzerrule.py
+++ generator/analyzerrule.py
@@ -8,33 +8,45 @@ Ivan Slijepcevic
 class AnalyzerRule():
     '''The model for the rule of the lexycal analyzer'''
     
-    def __init__( self, state_name, regular_expression, actions ):
+    def __init__( self, state_name, regular_expression, arguments ):
         '''constructor of the class
         
         arguments:
         state_name - the name of the state automat was in before rule is applied
         regular_expression - the regular expression object, for the analyzed program,
             that indicates when rule should be applied
-        actions - dictionary of actions to complete, possible keys:
-            'lexical_unit'
-            'new_line'
-            'change_state_to'
-            'go_back'
-            VALUES IMPLEMENTATION IS YET TO BE DECIDED
+        arguments - list of arguments with their corresponding actions
         '''
         
         self.state_name = state_name
         
         self.regular_expression = regular_expression
         
-        # possible example of implementation
         self.lexical_unit = None
         self.new_line = False
         self.change_state_to = None
         self.go_back = None
         
-        # mak, umjesto ovoga gore, trebas pravilno postaviti ove varijable
-        # sam odredi kako i iz cega, jer sam ces poslati argumente iz parsera
-        # kad budes stvarao objekte ove klase
+        self._parse_arguments (arguments)
 
+        
+    def _parse_arguments (self, arguments):
+        self.lexical_unit = arguments.pop(0)
+        if len (arguments) != 0:
+            for arg in arguments:
+                aname= arg.split()[0]
+                
+                if aname == 'UDJI_U_STANJE':
+                    self.change_state_to = arg.split()[1]
+                elif aname == 'NOVI_REDAK':
+                    self.new_line = True
+                elif aname == 'VRATI_SE':
+                    self.go_back = arg.split()[1]
+    
+    def toString(self):
+        return  ("---\ncurrentState: " + self.state_name + "\nregex: " +
+                self.regular_expression +
+                "\nLex unit: " + self.lexical_unit + '\nnew line: ' +
+                str(self.new_line) + '\nchangeState: ' + str(self.change_state_to) +
+                '\ngo Back: ' + str(self.go_back) + '\n---')
     #mak - slobodno dodaj funkcije koje ti trebaju u ovu klasu.
