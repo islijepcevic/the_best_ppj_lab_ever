5ed7a944a7022cb241b1fb6f74b203e9d5d45074
Goran <Gogo@(none)>
2011-12-03 03:56:08 +0100

    ispravljena funkcija izrade tablica

--- generator/model_analizatora.py
+++ generator/model_analizatora.py
@@ -13,7 +13,7 @@ class ModelAnalizatora:
         self.gramatika = gramatika
         self.automat = None
         
-        self.akcija = None  # u osnovi ovo je niz (lista)
+        self.akcija = []  # u osnovi ovo je niz (lista)
                             # svaki element liste je rjecnik (dict)
                             # svaki taj dict ima kljuceve koji su zavrsni
                             # znakovi gramatike (string), a vrijednost ce biti objekt
@@ -24,7 +24,7 @@ class ModelAnalizatora:
                             # pretpostavlja se da su stanja DKA prirodni brojevi 
                             # (ukljucujuci nulu)
         
-        self.novo_stanje = None # ova tablica je slicna kao akcija, dakle niz
+        self.novo_stanje = [] # ova tablica je slicna kao akcija, dakle niz
                                 # s indexom istim kao za stanje DKA;
                                 # clan niza je dict
                                 # kljuc dicta je zavrsni znak gramatike (string)
@@ -32,13 +32,7 @@ class ModelAnalizatora:
                                 # on oznacava koje se stanje stavlja kao novo stanje
                                 # (to je valjda jasno iz predavanja, a i iz samog imena tablice)
         
-        # Gorane, ne trebas za svaki redak i stupac tablica dodavati akciju
-        # ako je neki redak potpuno prazan, neka ostane prazan dicta
-        # ako u nekom retku za neki stupac nema akcije, neka ne postoji taj kljuc
-        # u dict. tako cemo lakse valjda i debugirati kasnije jer ce se zbog neceg 
-        # program skrsiti ako se nesto krivo izvelo (a u slucaju da svim nepotrebnima
-        # stavis npr None za vrijednost, mozda bi se nesto viska izvelo i teze bi 
-        # pronasli gresku) - ovo su samo pretpostavke, ali radimo tako
+
         
         self._stvori_tablice()
     
@@ -93,45 +87,58 @@ class ModelAnalizatora:
 
     
     
-    def _izgradi_tablice( self ):
 
-        # grdji kaos je slaganje ove funkcije, neka ideja je tu
-        # ALI ispravnost i izvedba i vise nego upitna
-        # budem se jos potrudio oko dorade ovoga kad budu gotovi i drugi dijelovi
-        # koji se izvrsavaju prije 
+
+    def _izgradi_tablice( self ):
 
 
         auto = self.automat
 
-        
         for s in range (len( auto.stanja )):
-            for i in range ( len( auto.prijelazi[s] )):
-                
-                if (( s, auto.prijelazi[s].desno_poslije_tocke[0] ) in auto.prijelazi):
-                    self.akcija[s, auto.prijelazi[s].desno_poslije_tocke[0]] = Akcija('pomakni',
-                    auto.prijelazi[( s, auto.prijelazi[s].desno_poslije_tocke[0] )])
 
-                if ( auto.prijelazi[s].desno_poslije_tocke == '' ):
-                    for a in range (len( auto.prijelazi[s].skup_zapocinje )):
-                        if auto.prijelazi[s].skup_zapocinje[a] == '<<novi_nezavrsni_znak>>':
+            self.akcija.append(dict())
+            self.novo_stanje.append(dict())
+
+
+            for i in range (len( auto.stanja[s] )):
+
+                if (( s, auto.stanja[s][i].desno_poslije_tocke[0]) in
+                    auto.prijelazi) and (not auto.stanja[s][i].desno_poslije_tocke[0].startswith('<')):
+                    kanter += 1
+
+                    self.akcija[s][auto.stanja[s][i].desno_poslije_tocke[0]] = Akcija ('pomakni',
+                    auto.prijelazi[(s, auto.stanja[s][i].desno_poslije_tocke[0])])
+
+                if auto.stanja[s][i].je_li_potpuna():
+                    
+                    if auto.stanja[s][i].lijeva_strana == '<<novi_nezavrsni_znak>>':
+                        
+                        if auto.stanja[s][i].skup_zapocinje == ['kraj_niza']:
+
+                            self.akcija[s][auto.stanja[s][i].skup_zapocinje[0]] = 'Potvrda()'
+      
+                        
                             continue
-                        self.akcija[s, auto.prijelazi[s].skup_zapocinje[a]] = Akcija('reduciraj',
-                        Produkcija(auto.prijelazi[s].lijeva_strana, auto.prijelazi[s].desno_prije_tocke ))
+                                           
 
-                #### ovo nemam pojma kak izvest
-                if ( auto.prijelazi[s].lijeva_strana == '<<novi_nezavrsni_znak>>' ):
-                    if auto.prijelazi[s].je_li_potpuna():
-                        self.akcija[s, 'kraj_niza'] = 'Prihvati()'
-                ####
+                    for x in range (len (auto.stanja[s][i].skup_zapocinje)):
+
+                        if auto.stanja[s][i].desno_prije_tocke == [''] :
+
+                            # da imamo lijepo zapisano, nece slat prazni skup nego onaj epsilon
+                            # u obliku znaka '$' bas kao u Ulaznoj!
+
+                            self.akcija[s][auto.stanja[s][i].skup_zapocinje[x]] = Akcija ('reduciraj',
+                            Produkcija( auto.stanja[s][i].lijeva_strana, '$' ))
+                            
+                        else:
+                            self.akcija[s][auto.stanja[s][i].skup_zapocinje[x]] = Akcija ('reduciraj',
+                            Produkcija( auto.stanja[s][i].lijeva_strana, auto.stanja[s][i].desno_prije_tocke ))
 
                         
-            
-            for x in range ( len( auto.ulazni_znakovi )):
-                '''trebalo bi mu poslat samo nezavrsne_ulazne ili na neki drugi
-                nacin to provjerit!
-                '''
-                if (s, auto.ulazni_znakovi[x]) in auto.prijelazi:
-                    self.novo_stanje[s, auto.ulazni_znakovi[x]] = auto.prijelazi[( s, auto.ulazni_znakovi[x])]
-                                      
-        
 
+            for x in range ( len ( auto.ulazni_znakovi ) ):
+                if ((s, auto.ulazni_znakovi[x]) in auto.prijelazi) and auto.ulazni_znakovi[x].startswith('<'):
+                    self.novo_stanje[s][auto.ulazni_znakovi[x]] = auto.prijelazi[(s, auto.ulazni_znakovi[x])]
+
+                    
