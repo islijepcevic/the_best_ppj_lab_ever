79984fcba8d3c7c38f56b358594ad540658cca6a
Ivan Slijepcevic <islijepcevic@gmail.com>
2011-12-04 03:23:10 +0100

    dodane metode za usporedjivanje instanci Produkcija

--- analizator/zajednicki/produkcija.py
+++ analizator/zajednicki/produkcija.py
@@ -17,3 +17,21 @@ class Produkcija:
         '''
         return 'Produkcija("' + self.lijeva_strana + '","' + \
                 repr( self.desna_strana ) + '")'
+    
+    
+    def __hash__( self ):
+        
+        za_hash = self.lijeva_strana
+        
+        for znak_gramatike in self.desna_strana:
+            za_hash += ':' + znak_gramatike 
+        
+        return hash( za_hash )
+    
+    
+    def __cmp__( self, other ):
+        
+        hash_self = self.__hash__()
+        hash_other = other.__hash__()
+        
+        return hash_self - hash_other
